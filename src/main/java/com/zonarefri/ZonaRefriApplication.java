403775759544ca8f4a7557e6d7b68b28943823f5
LuluLudmi <gilmaaguadajura@gmail.com>
2026-01-12 22:43:10 -0300

    Tarea finalizada y lista para revisión

---
+++ src/main/java/com/zonarefri/ZonaRefriApplication.java
@@ -0,0 +1,13 @@
+package com.zonarefri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaRefriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonaRefriApplication.class, args);
+    }
+
+}
